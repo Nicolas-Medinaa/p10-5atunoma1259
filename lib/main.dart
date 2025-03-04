6054d8e61ba2289d5f0c39718cb8f3d975d3bb93
NICOLAS_MEDINA_ZUBIA <a.22308051281259@cbtis128.edu.mx>
2025-03-04 17:03:41 +0000

    otra vez

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,108 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiContainerApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiContainerApp extends StatelessWidget {
+  const MiContainerApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Nicolas Medina 1259"),
+          titleTextStyle:
+              const TextStyle(color: Color(0xff000000), fontSize: 20),
+          centerTitle: true,
+          backgroundColor: Color(0xff00c4ff),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Container(
+                height: 95,
+                color: Color(0xffff4c3f),
+                margin: const EdgeInsets.only(bottom: 16.0),
+                child: Center(
+                  child: Text(
+                    'Contenedor Rojo',
+                    style: TextStyle(
+                      color: Color(0xff000000),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 95,
+                color: Color(0xffed8888),
+                margin: EdgeInsets.only(bottom: 16.0),
+                child: Center(
+                  child: Text(
+                    'Contenedor rosa',
+                    style: TextStyle(
+                      color: Color(0xff000000),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 95,
+                color: Color(0xff5c5c5c),
+                margin: EdgeInsets.only(bottom: 16.0),
+                child: Center(
+                  child: Text(
+                    'Contenedor gris',
+                    style: TextStyle(
+                      color: Color(0xff000000),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 95,
+                color: Color(0xfff4ba19),
+                margin: EdgeInsets.only(bottom: 16.0),
+                child: Center(
+                  child: Text(
+                    'Contenedor amarillo',
+                    style: TextStyle(
+                      color: Color(0xff000000),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 95,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xff427eff), Colors.lightBlueAccent],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    'Contenedor Degradado',
+                    style: TextStyle(
+                      color: Color(0xff000000),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
